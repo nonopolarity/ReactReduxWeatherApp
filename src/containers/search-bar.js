b38da8f0350f6760cb925c0639cf87cd20c4f71c
Kin <winterheat@gmail.com>
2017-03-30 06:05:33 -0700

    add search bar as container

---
+++ src/containers/search-bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+
+  render() {
+    return (
+      <form>
+        <input /><button>Search</button>
+      </form>
+    );
+
+  }
+
+}
