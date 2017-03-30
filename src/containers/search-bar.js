f6053c1bb1f006d30bd3db1b7ea75d724bc336cc
Kin <winterheat@gmail.com>
2017-03-30 08:00:40 -0700

    make the input field a 'controlled field', reflecting the component state all the time

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -2,10 +2,27 @@ import React, { Component } from "react";
 
 export default class SearchBar extends Component {
 
+  constructor(props) {
+    console.log("Inside SearchBar constructor");
+    super(props);
+    this.state = { term: "" }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <form className="input-group">
-      <input className="form-control" placeholder="Search for..."/>
+      <input
+        className="form-control"
+        placeholder="Search for..."
+        value={this.state.term}
+        onChange={this.handleInputChange}
+      />
       <span className="input-group-btn">
         <button className="btn btn-secondary" type="submit">Search</button>
         </span>
